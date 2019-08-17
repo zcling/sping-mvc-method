c95c8d542e34ed9b2c15cbbfffd058ba618e42d8
zcling <361526993@qq.com>
2019-08-17 17:28:36 +0800

    主要用于测试提交//2019/08/19

--- src/main/java/com/zcl/AppMain.java
+++ src/main/java/com/zcl/AppMain.java
@@ -23,7 +23,7 @@ public class AppMain {
         System.out.println(student.toString());
 */
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
-        User user = (User) applicationContext.getBean(" t getUser");
+        User user = (User) applicationContext.getBean("ceshi fenzhi  t getUser");
     }
 
     @Test
